44adc2bfe6d37e168edf6e14d1750d5f6c20585f
Kevin Kang & Aki Suzuki <pair+kevinkang88.ocktagon@devbootcamp.com>
2014-07-05 14:56:43 -0700

    CasesController logic updated to create case and add users to case

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -4,8 +4,9 @@ class CasesController < ApplicationController
   end
 
   def create
-    Case.create(title: title[:params], summary: summary[:params])
-    CasesUser.create(party: session[:id])
+    new_case = Case.create(title: params[:title], summary: params[:summary])
+    CasesUser.create(case_id: new_case.id, user_id: session[:id], party: "plaintiff")
+    CasesUser.create(case_id: new_case.id, user_id: User.find_by_user_name(params[:defendant_user_name]).id, party: "defendant")
     redirect_to dashboard_path
   end
 end
