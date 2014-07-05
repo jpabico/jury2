82d4d5882d1ea52d65d080bea914a7ccccc003ef
Adam Ryssdal & Jon Pabico <pair+aryssdal.jpabico@devbootcamp.com>
2014-07-05 15:55:25 -0700

    Updated Case show view, updated seed.rb file, updated evidence model

--- app/models/evidence.rb
+++ app/models/evidence.rb
@@ -1,10 +1,10 @@
 class Evidence < ActiveRecord::Base
-  attr_accessible :argument, :mediatype, :party, :users_cases_id
-  belongs_to :users_cases
+  attr_accessible :argument, :mediatype, :party, :cases_user_id
+  belongs_to :cases_user
 
   validates :argument, presence: true
   validates :mediatype, presence: true
   validates :party, presence: true
 
-  validates :users_cases_id, presence: true
+  validates :cases_user_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+def seedTheDatabase
+  User.create(user_name: "Jon", email: "jon@jon.com")
+  User.create(user_name: "Adam", email: "adam@adam.com")
+
+  newCase = Case.create(title: "A Test Case")
+  User.find(1).cases << newCase
+  User.find(2).cases << newCase
+  Evidence.create(argument: "This is the winning argument. There can be no other possible solution to this argument that makes any sense at all. Blah blah blah blah. More argument stuff here - blah blah blah blah", mediatype: "written", party: "1", cases_user_id: '1')
+   Evidence.create(argument: "This is the other winning argument. Blah blah blah blah", mediatype: "written", party: "2", cases_user_id: '2')
+ end
+
+ seedTheDatabase
+
+
