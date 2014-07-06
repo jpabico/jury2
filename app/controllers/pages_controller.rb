984dde17bdc17901b4e6baa31ed9bc0898fee747
Aki Suzuki <akisuzucky@gmail.com>
2014-07-05 18:34:11 -0700

    Added counts, challenge alerts and links to Dashboard view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,11 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @my_cases = User.find(session[:id]).cases.where(status: "pending")
+  	@target_user = User.find(session[:id])
+
+  	@my_cases = User.find(session[:id]).cases
+  	@my_invites = User.find(session[:id]).cases.where(status: "pending")
+    
     @active_cases = Case.where(status: "active")
 		@closed_cases = Case.where(status: "closed")
   end
