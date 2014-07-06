3de56326ccde6e55491c564b1a3e78e03f0686b6
Aki Suzuki <akisuzucky@gmail.com>
2014-07-06 13:07:51 -0700

    Fixed error where alerts were displaying for current user - Handled error by adding if statements to dashboard.html. - Added display to say no alerts are here when invite_count = 0 - Added total counts to each section of cases

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,7 @@ class PagesController < ApplicationController
   	@target_user = User.find(session[:id])
 
   	@my_cases = User.find(session[:id]).cases
+
   	@my_invites = User.find(session[:id]).cases.where(status: "pending")
     
     @active_cases = Case.where(status: "active")
