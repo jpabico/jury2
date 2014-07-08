c0e47fe73ef3c7cc4ba80c73ea516e7730176788
Aki Suzuki <akisuzucky@gmail.com>
2014-07-08 08:47:20 -0700

    MVP - Completed google signin, video upload for case WOOT! Cleaned up partials for casebody.html.  Page now displays video! Changed one case with MJ on it.  Billie Jean is not my lover... All video playback modules under google_video.js

--- app/assets/javascripts/google_video.js
+++ app/assets/javascripts/google_video.js
@@ -12,7 +12,7 @@ var widget;
 var player;
 function onYouTubeIframeAPIReady() {
   widget = new YT.UploadWidget('widget', {
-    width: 500,
+    width: 400,
     events: {
       'onUploadSuccess': onUploadSuccess,
       'onProcessingComplete': onProcessingComplete
@@ -23,14 +23,35 @@ function onYouTubeIframeAPIReady() {
 // 4. This function is called when a video has been successfully uploaded.
 function onUploadSuccess(event) {
   alert('Video ID ' + event.data.videoId + ' was uploaded and is currently being processed.');
+  $('#video-status').text('Video is processing... wait for it...')
 }
 
 // 5. This function is called when a video has been successfully processed.
 function onProcessingComplete(event) {
+  $('#widget').hide();
+  $('#video_url').val(event.data.videoId);
+  $('#video-status').text('Time for a preview!')
   player = new YT.Player('player', {
-    height: 390,
-    width: 640,
+    height: 282,
+    width: 500,
     videoId: event.data.videoId,
     events: {}
   });
 }
+
+// For the player to display on cases page
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replace the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+  var casePlayer;
+  function onYouTubePlayerAPIReady() {
+    casePlayer = new YT.Player('ytplayer', {
+      height: 390,
+      width: 640,
+      videoId: 'M7lc1UVf-VE'
+    });
+  }
