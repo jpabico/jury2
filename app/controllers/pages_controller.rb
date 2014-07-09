6f4ed187dbd54df4569b30f523ce82fef93dbe24
Wm. Butler Bushyhead <Butler@Williams-MacBook-Pro.local>
2014-07-09 11:10:59 -0700

    Cleaned up pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,17 +4,11 @@ class PagesController < ApplicationController
     @user = User.new
   end
 
-  def test
-
-  end
-
   def dashboard
     if session[:id]
   	  @target_user = User.find(session[:id])
       @my_cases = User.find(session[:id]).cases.order('status').order('created_at')
-
       @my_invites = User.find(session[:id]).cases.where(status: "pending")
-
     end
       @active_cases = Case.where(status: "active")
       @closed_cases = Case.where(status: "closed")
