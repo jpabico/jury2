12996d32c95beab2743ed02cfd9d357e7746271f
Aki Suzuki <akisuzucky@gmail.com>
2014-07-08 20:19:05 -0700

    Bug fix for google signin/signout controller.  Page should now work for both evidence new and response to alert.

--- app/controllers/google_controller.rb
+++ app/controllers/google_controller.rb
@@ -1,30 +1,36 @@
 class GoogleController < ApplicationController
   def create
 
-    if params[:case_id] != "" || params[:case_id] != nil
+    if session[:case_user_plaintiff]
+      #When user is coming from a create new case/evidence (starting a new case)
+      user = User.from_omniauth(env["omniauth.auth"])
+      session[:user_id] = user.id
+      # Redirect back to original plaintiff url
+      redirect_to :controller => 'evidences', :action => 'new', :case_user_plaintiff => session[:case_user_plaintiff]
+    else
+      # When user is coming from a case page (responding to a new case)
       user = User.from_omniauth(env["omniauth.auth"])
       session[:user_id] = user.id
       # Redirect back to case/:id page user was on
       redirect_to case_path(session[:case_id])      
-    elsif params[:case_id] == "" || params[:case_id] == nil
-      user = User.from_omniauth(env["omniauth.auth"])
-      session[:user_id] = user.id
-      # Redirect back to original plaintiff url
-      redirect_to :controller => 'evidences', :action => 'new', :case_user_plaintiff => session[:case_user_plaintiff]
     end
+
   end
+
 
   def destroy
 
-    if params[:case_id] != "" || params[:case_id] != nil
-      session[:user_id] = nil
-      # Redirect back to case/:id page user was on
-      redirect_to case_path(session[:case_id])
-    elsif params[:case_id] == "" || params[:case_id] == nil
-      # Redirect back to original plaintiff url
+    if session[:case_user_plaintiff]
       session[:user_id] = nil
       redirect_to :controller => 'evidences', :action => 'new', :case_user_plaintiff => session[:case_user_plaintiff]
+     
+    else
+      session[:user_id] = nil
+      # Redirect back to case/:id page user was on
+      redirect_to case_path(session[:case_id])    
     end
+
   end
+
 end
 
