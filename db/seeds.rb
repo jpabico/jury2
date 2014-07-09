1dc611327ebeac9bbf343e9bb2033c1b508758f8
Aki Suzuki <akisuzucky@gmail.com>
2014-07-09 12:02:46 -0700

    Updated CRON rake:dbupdate task logic to run based on active_end in case.  Updated seed file to include variable active_end dates for the active cases.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
-countdown_time = 10.minutes
+countdown_time1 = 1.minutes
+countdown_time2 = 10.minutes
+countdown_time3 = 12.hours
+countdown_time4 = 1.days
+countdown_time5 = 2.days
 
 puts "Test users seeded ----------->>>"
 User.create(user_name: "akimeister", email: "aki@test.com", password: "123")
@@ -9,7 +13,7 @@ User.create(user_name: "kingadam", email: "kingadam@test.com", password: "123")
 
 puts "Creating some active cases ----------->>>"
 # akimeister v. donkeykang - active case
-Case.create(title: "The Kid is Not My Son", summary: "Billie Jean is not my lover.... she took my soul and said that I was the one. But...", status: "active", active_start: Time.now, active_end: Time.now + countdown_time)
+Case.create(title: "The Kid is Not My Son", summary: "Billie Jean is not my lover.... she took my soul and said that I was the one. But...", status: "active", active_start: Time.now, active_end: Time.now + countdown_time1)
 CasesUser.create(case_id: 1, user_id: 1, party: "plaintiff")
 CasesUser.create(case_id: 1, user_id: 2, party: "defendant")
 Evidence.create(argument: "Look, there's just no doubt that MJ is truly awesome.  Vote for me!", video_url: "Zi_XLOBDo_Y", cases_user_id: 1)
@@ -21,31 +25,31 @@ Comment.create(body:"Oh come on guys.  The real question is whether he is black 
 Comment.create(body:"Point taken.", user_id:5,case_id:1)
 Comment.create(body:"We shouldn't be talking about MJ.  Time to open a case for Teenage Mutant Ninja Turtles!", user_id:4,case_id:1)
 # basher v. papajons
-Case.create(title: "You are NOT a supermodel", summary: "Pabico thinks he's so sexy, but not so much says I.  Like seriously.", status: "active", active_start: Time.now, active_end: Time.now + countdown_time)
+Case.create(title: "You are NOT a supermodel", summary: "Pabico thinks he's so sexy, but not so much says I.  Like seriously.", status: "active", active_start: Time.now, active_end: Time.now + countdown_time1)
 CasesUser.create(case_id: 2, user_id: 4, party: "plaintiff")
 CasesUser.create(case_id: 2, user_id: 3, party: "defendant")
 Evidence.create(argument: "papajons is not a super model.  He needs to perfect his cat walk!", video_url: "hUcqPQizglE", cases_user_id: 3)
 Evidence.create(argument: "papajons is a super model.  His walk is so fierce!", video_url: "AfADZSvC0Hk", cases_user_id: 4)
 # kingadam v donkeykang
-Case.create(title: "Transformers are better than G.I. Joe", summary: "Transformers are so awesome.  After all, they do have Optimus Prime and Megatron.  So much better than G.I. Joe", status: "active", active_start: Time.now, active_end: Time.now + countdown_time)
+Case.create(title: "Transformers are better than G.I. Joe", summary: "Transformers are so awesome.  After all, they do have Optimus Prime and Megatron.  So much better than G.I. Joe", status: "active", active_start: Time.now, active_end: Time.now + countdown_time2)
 CasesUser.create(case_id: 3, user_id: 5, party: "plaintiff")
 CasesUser.create(case_id: 3, user_id: 2, party: "defendant")
 Evidence.create(argument: "Transformers is an entertainment franchise co-produced between the Japanese Takara Tomy and American Hasbro toy companies. So awesome.", video_url: "P7GeisRaias", cases_user_id: 5)
 Evidence.create(argument: "Nothing better than G.I. Joe the movie.  An elite military unit comprised of special operatives known as G.I. Joe, operating out of The Pit, takes on an evil organization led by a notorious arms dealer.", video_url: "ogEtfIdgjpY", cases_user_id: 6)
 
-Case.create(title: "The dude says GIT too much in this Git tutorial video", summary: "Puns are funny but he should use them sparingly.  Like GIT real.  It's GITTING hot in here!", status: "active", active_start: Time.now, active_end: Time.now + countdown_time)
+Case.create(title: "The dude says GIT too much in this Git tutorial video", summary: "Puns are funny but he should use them sparingly.  Like GIT real.  It's GITTING hot in here!", status: "active", active_start: Time.now, active_end: Time.now + countdown_time3)
 CasesUser.create(case_id: 4, user_id: 3, party: "plaintiff")
 CasesUser.create(case_id: 4, user_id: 1, party: "defendant")
 Evidence.create(argument: "This is the offending GIT video.  Don't you think he says GIT too much?  GIT it!!!???", video_url: "tRTckrrCME4", cases_user_id: 7)
 Evidence.create(argument: "I disagree.  Always good to emphasize GIT.  Just remember to not push to master...  GIT ready!!!", video_url: "o3Z8NU5ImK0", cases_user_id: 8)
 
-Case.create(title: "Pair programming is the best!", summary: "Pair programming is an agile software development technique in which two programmers work together at one workstation. The driver writes code while the navigator talks a lot.  Two programmers switch roles frequently.", status: "active", active_start: Time.now, active_end: Time.now + countdown_time)
+Case.create(title: "Pair programming is the best!", summary: "Pair programming is an agile software development technique in which two programmers work together at one workstation. The driver writes code while the navigator talks a lot.  Two programmers switch roles frequently.", status: "active", active_start: Time.now, active_end: Time.now + countdown_time4)
 CasesUser.create(case_id: 5, user_id: 5, party: "plaintiff")
 CasesUser.create(case_id: 5, user_id: 2, party: "defendant")
 Evidence.create(argument: "Pair programming is awesome.  Take a look at the best practices.", video_url: "ET3Q6zNK3Io", cases_user_id: 9)
 Evidence.create(argument: "Pair programming is so not awesome.  I don't like following best practice.  But you are the best... around!", video_url: "9fWvub_WBho", cases_user_id: 10)
 
-Case.create(title: "J-POP is better than K-POP.  I challenge u.", summary: "J-POP is so much better than K-POP.  What do you think?", status: "active", active_start: Time.now, active_end: Time.now + countdown_time)
+Case.create(title: "J-POP is better than K-POP.  I challenge u.", summary: "J-POP is so much better than K-POP.  What do you think?", status: "active", active_start: Time.now, active_end: Time.now + countdown_time5)
 CasesUser.create(case_id: 6, user_id: 1, party: "plaintiff")
 CasesUser.create(case_id: 6, user_id: 2, party: "defendant")
 Evidence.create(argument: "Just look at this vid from Hikaru Utada.  How is that not awesome?", video_url: "cfpX8lkaSdk", cases_user_id: 11)
