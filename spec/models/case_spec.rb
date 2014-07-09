1d37073b5b5fcaf700789baef0517a673d54544c
Apprentice <apprentice@master-inf-2.local>
2014-07-09 15:25:53 -0700

    Fixed test suite to match changes in database

--- spec/models/case_spec.rb
+++ spec/models/case_spec.rb
@@ -7,18 +7,11 @@ describe Case do
     expect(my_case).to be_valid
   end
 
-  it "sets user vote counts to zero when case is initiated" do
-    expect(my_case.user_1_vote_count).to eq 0
-    expect(my_case.user_2_vote_count).to eq 0
-  end
 
   it "is invalid without a title" do
     expect(Case.new(title: nil)).to have(1).errors_on(:title)
   end
 
-  it "is invalid without a status" do
-    expect(Case.new(status: nil)).to have(1).errors_on(:status)
-  end
 
 
 end
