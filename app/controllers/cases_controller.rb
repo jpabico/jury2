2e10069d049f194c04fa35c65e0214d52919d6a9
Kevin <apprentice@master-inf-4.local>
2014-07-08 19:00:12 -0700

    no more errors when typing in defendant who are not user

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -4,10 +4,14 @@ class CasesController < ApplicationController
   # end
 
   def create
+    if User.find_by_user_name(params["defendant_user_name"]) == nil
+      redirect_to new_case_path
+    else
     new_case = Case.create(title: params[:title], summary: params[:summary])
     @case_user_plaintiff = CasesUser.create(case_id: new_case.id, user_id: session[:id], party: "plaintiff")
     CasesUser.create(case_id: new_case.id, user_id: User.find_by_user_name(params[:defendant_user_name]).id, party: "defendant")
     redirect_to new_evidence_path(:case_user_plaintiff => @case_user_plaintiff)
+    end
   end
 
   def show
