e6f3c7da8c7777b7381f1d2f14f20eef11c1068c
Wm. Butler Bushyhead <Butler@Williams-MacBook-Pro.local>
2014-07-09 11:02:38 -0700

    Refactored the ballots and cases controllers - moved ballot creation out of the conditional - added private methods for finding parms for case and case_user creations

--- app/controllers/ballots_controller.rb
+++ app/controllers/ballots_controller.rb
@@ -3,14 +3,11 @@ class BallotsController < ApplicationController
   respond_to :json
 
   def create
+    Ballot.create(user_id: session[:id], case_id: params[:case_id], role: params[:role])
     if params[:role] == "defendant"
-      Ballot.create(user_id: session[:id], case_id: params[:case_id], role: params[:role])
       @myresult =  Ballot.where(:role => "defendant", :case_id => params[:case_id]).count
-
     elsif params[:role] == "plaintiff"
-      Ballot.create(user_id: session[:id], case_id: params[:case_id], role: params[:role])
       @myresult =  Ballot.where(:role => "plaintiff", :case_id => params[:case_id]).count
-
     end
     render json: @myresult
 
--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -7,17 +7,29 @@ class CasesController < ApplicationController
     if User.find_by_user_name(params["defendant_user_name"]) == nil
       redirect_to new_case_path
     else
-    new_case = Case.create(title: params[:title], summary: params[:summary])
-    @case_user_plaintiff = CasesUser.create(case_id: new_case.id, user_id: session[:id], party: "plaintiff")
-    CasesUser.create(case_id: new_case.id, user_id: User.find_by_user_name(params[:defendant_user_name]).id, party: "defendant")
-    redirect_to new_evidence_path(:case_user_plaintiff => @case_user_plaintiff)
+      get_case_params
+      @new_case = Case.create(@case_params)
+      get_case_user_params("plaintiff")
+      @case_user_plaintiff = CasesUser.create(@case_user_params)
+      get_case_user_params("defendant")
+      CasesUser.create(@case_user_params)
+      redirect_to new_evidence_path(:case_user_plaintiff => @case_user_plaintiff)
     end
   end
 
   def show
     @case = Case.find(params[:id])
     session[:case_id] = params[:id]
+  end
 
+  private
+
+  def get_case_params
+    @case_params = {title: params[:title], summary: params[:summary]}
+  end
+
+  def get_case_user_params(party)
+    @case_user_params = {case_id: @new_case.id, user_id: session[:id], party: party}
   end
 
 end
