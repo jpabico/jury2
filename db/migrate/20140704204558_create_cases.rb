4591ed7050dfd88127bc3a59c9fdbafda30c7c59
Aki Suzuki <akisuzucky@gmail.com>
2014-07-09 10:30:38 -0700

    added default values nil to active_end/active_start in Case migration

--- db/migrate/20140704204558_create_cases.rb
+++ db/migrate/20140704204558_create_cases.rb
@@ -5,8 +5,8 @@ class CreateCases < ActiveRecord::Migration
       t.string :summary
       t.string :status, default: "pending"
       t.string :winner, default: nil
-      t.datetime :active_start
-      t.datetime :active_end      
+      t.datetime :active_start, default: nil
+      t.datetime :active_end, default: nil      
       t.timestamps
     end
   end
