bf4cf71368780c54afa543cddb8430f2cc47905e
Wm. Butler Bushyhead <Butler@Williams-MacBook-Pro.local>
2014-07-08 21:06:00 -0700

    Vote Ajax almost done, can get needed info back but need to supress it's diplaying. Consolidated Butler and Adams JS into one version that is name spaced.

--- app/assets/javascripts/cases.js
+++ app/assets/javascripts/cases.js
@@ -1,17 +1,39 @@
 $(document).ready(function() {
-
-
-  $('.vote-defendant').on("ajax:success", function(e, data, status, xhr){
-    $("#defendant_vote_count").text(data);
-  });
-   $('.vote-plaintiff').on("ajax:success", function(e, data, status, xhr){
-    $("#plaintiff_vote_count").text(data);
-  });
-   $('.commentsBox').on("ajax:success", function(e, data, status, xhr){
-      $('.commentsBox').append(data)
-  });
+  bindEvents();
  })
 
+function bindEvents(){
+
+  $('.vote-defendant').on("ajax:success", addDefendant)
+  $('.vote-plaintiff').on("ajax:success",addPlaintiff)
+  $('.commentsBox').on("ajax:success", addComment)
+  $(document).on('ajax:success', '#like', updateVote)
+  $(document).on('ajax:success', '#dislike', updateVote)
+  // $(document).on('ajax:error', '#dislike', wtf)
+}
+    function addComment(e, data, status, xhr){
+      $('.commentsBox').append(data)
+    };
+
+    function addPlaintiff(e, data, status, xhr){
+      $("#plaintiff_vote_count").text(data);
+    };
+
+    function addDefendant(e, data, status, xhr){
+      $("#defendant_vote_count").text(data);
+    };
+
+    function updateVote(e, data, status, xhr){
+      var x = "." + data[2] + "up"
+      var y = "." + data[2] + "down"
+      $(x).text(data[0])
+      $(y).text(data[1])
+      console.log($(x))
+    }
+
+    function wtf(){
+      console.log('wtf')
+    }
 
 
 
