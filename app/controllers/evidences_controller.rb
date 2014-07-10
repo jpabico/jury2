b8f3c52e4d88525ca4b430e184ec89dfd19a26b3
Kevin <apprentice@master-inf-4.local>
2014-07-09 19:24:32 -0700

    fixed a small bug after merging two css files

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -22,7 +22,7 @@ class EvidencesController < ApplicationController
         photo_url: params["photo_url"],
         cases_user_id: params["case_user_plaintiff"]
       })
-
+    end
     if params[:case_id] == "" || params[:case_id] == nil
       @evidence = Evidence.create(argument: params["argument"], video_url: params["video_url"], photo_url: params["photo_url"], cases_user_id: params[:case_user_plaintiff])
 
