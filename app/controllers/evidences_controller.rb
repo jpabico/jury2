de6161c690d00f4f8fdaeffa2556ad19efba56f7
Kevin <apprentice@master-inf-4.local>
2014-07-09 19:09:52 -0700

    changed layout and finished create case workflow

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -16,7 +16,6 @@ class EvidencesController < ApplicationController
 
   def create
 
-
     if params[:case_id] == "" || params[:case_id] == nil
       @evidence = Evidence.create(argument: params["argument"], video_url: params["video_url"], photo_url: params["photo_url"], cases_user_id: params[:case_user_plaintiff])
       redirect_to dashboard_path
@@ -26,6 +25,7 @@ class EvidencesController < ApplicationController
       Case.update(params[:case_id], active_start: Time.now)
 
       redirect_to dashboard_path
+
 
     end
   end
