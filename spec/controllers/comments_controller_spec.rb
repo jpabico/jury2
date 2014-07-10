aff2cbcd67e295f3afceda1311d4b6ac4bf5fb58
Wm. Butler Bushyhead <Butler@Williams-MacBook-Pro.local>
2014-07-09 19:46:56 -0700

    removed tests that weren't testing anything

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -15,32 +15,6 @@ describe CommentsController do
         }.to change(Comment, :count).by(1)
       end
     end
+
   end
-
-    context "given a session id" do
-      it "renders partial new_comment" do
-        expect {
-          session[:id] = 123
-          post :create, {
-            comment: { body: "text" },
-            case_id: 1
-          }
-        }.to render_template(partial: "new_comment",
-                            locals: { comment: "test",
-                            username: "test" })
-      end
-    end
-
-    context "given no session id" do
-      it "renders status 404" do
-        expect {
-          session[:id] = nil
-          post :create, {
-            comment: { body: "text" },
-            case_id: 1
-          }
-        }.to render_template(json: { status: 404 })
-      end
-    end
-
 end
